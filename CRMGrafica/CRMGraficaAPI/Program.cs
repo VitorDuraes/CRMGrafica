3c461eaefe48b80a1ed22ecd7b00e3b74c3738a2
Vitor Durães <111949857+VitorDuraes@users.noreply.github.com>
2025-06-21 00:15:20 -0300

    feat:fazendo que a migration atualize

--- CRMGrafica/CRMGraficaAPI/Program.cs
+++ CRMGrafica/CRMGraficaAPI/Program.cs
@@ -40,5 +40,11 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    dbContext.Database.Migrate();
+}
+
 app.Run();
 
